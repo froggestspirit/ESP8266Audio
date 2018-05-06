f392c14200b4ed647804bfb87dcb83171b6fd868
froggestspirit <froggestspirit@gmail.com>
2018-05-06 12:37:38 -0400

    Initial Commit

---
+++ examples/PlaySDAT/PlaySDAT.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#ifdef ESP32
+    #include <WiFi.h>
+#else
+    #include <ESP8266WiFi.h>
+#endif
+
+
+#include <AudioOutputI2S.h>
+#include <AudioGeneratorNDS.h>
+#include <AudioFileSourcePROGMEM.h>
+
+#include "sdat.h"
+
+AudioGeneratorNDS *nds;
+AudioFileSourcePROGMEM *sdat;
+AudioOutputI2S *dac;
+int song=30;
+
+void setup()
+{
+  WiFi.mode(WIFI_OFF); 
+
+  Serial.begin(115200);
+  Serial.println("Starting up...\n");
+
+  sdat = new AudioFileSourcePROGMEM(sdatFile, sizeof(sdatFile));
+  dac = new AudioOutputI2S();
+  nds = new AudioGeneratorNDS();
+  Serial.printf("BEGIN...\n");
+  nds->begin(sdat, dac, song);
+}
+
+void loop()
+{
+  if (nds->isRunning()) {
+    if (!nds->loop()) {
+      uint32_t e = millis();
+      nds->stop();
+    }
+  } else {
+    song++;
+    nds->begin(sdat, dac, song);
+    Serial.println(song);
+    delay(1000);
+  }
+}
+
